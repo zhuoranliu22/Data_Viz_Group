c1c684de2c8d9594ecec5b5ad9e038ef49fc3aaf
zhuoranliu22 <zhuoran.liu.22@ucl.ac.uk>
2023-05-09 17:39:53 +0100

    background changed

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoiemh1b3JhbmxpdSIsImEiOiJjbGQxbDR6M2QyN2s5M3Bud
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/light-v11',
     center: [-74.0060, 40.7128], // Set the initial map center (longitude, latitude)
     zoom: 10
 });
 
 
+
+
 map.on('load', () => {
+    // map.addLayer({
+    //     id: 'background',
+    //     type: 'background',
+    //     paint: {
+    //         'background-color': 'rgba(128,128,128,0.7)' // Grey color
+    //         // 'background-opacity': 0.5
+    //     }
+        
+    // });
+
     map.addSource('data_map', {
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/zhuoranliu22/Data_Viz_Group/main/data_group_viz.json'
@@ -23,11 +35,11 @@ map.on('load', () => {
             'fill-color': [
                 'match',
                 ['get', 'DsplcRI'],
-                'Lowest', '#F2F12D',
-                'Lower', '#EED322',
-                'Intermediate', '#E6B71E',
-                'Higher', '#DA9C20',
-                'Highest', '#CA8323',
+                'Lowest', '#B3CDE0',
+                'Lower', ' #6897BB',
+                'Intermediate', '#1F78B4',
+                'Higher', '#053061',
+                'Highest', '#011627',
                 '#000000' // fallback color if none of the categories match
             ],
             'fill-opacity': 0.8
@@ -45,30 +57,32 @@ map.on('load', () => {
             'fill-color': [
                 'match',
                 ['get', 'HsngCnd'],
-                'Lowest', '#F2F12D',
-                'Lower', '#EED322',
-                'Intermediate', '#E6B71E',
-                'Higher', '#DA9C20',
-                'Highest', '#CA8323',
+                'Lowest', '#D5E8D4',
+                'Lower', '#92C47D',
+                'Intermediate', '#6AA84F',
+                'Higher', '#38761D',
+                'Highest', '#274E13',
                 '#000000' // fallback color if none of the categories match
             ],
-            'fill-opacity': 0.8
+            'fill-opacity': 0
             
             
         }
     });
 });
 
+
 document.getElementById("displacement").addEventListener("click", function(){
-map.setPaintProperty('DsplcRI','fill-opacity',1);
-map.setPaintProperty('HsngCnd','fill-opacity',0);
+    map.setPaintProperty('displacement','fill-opacity',1);
+    map.setPaintProperty('housing','fill-opacity',0);
 });
     
 document.getElementById("housing").addEventListener("click", function(){
-map.setPaintProperty('DsplcRI','fill-opacity',0);
-map.setPaintProperty('HsngCnd','fill-opacity',1);
+    map.setPaintProperty('displacement','fill-opacity',0);
+    map.setPaintProperty('housing','fill-opacity',1);
 });
-    
+
+
 
 const popup = new mapboxgl.Popup({
     closeButton: false,
@@ -141,3 +155,54 @@ map.on('mouseleave', 'displacement', () => {
 });
 
 
+
+map.on('click', 'housing', (e) => {
+    // Get the properties of the clicked feature
+    const properties = e.features[0].properties;
+    // Generate the content for the info panel
+    let infoPanelContent = '<h3>Feature Information</h3><ul>';
+    for (const property in properties) {
+        infoPanelContent += `<li>${property}: ${properties[property]}</li>`;
+    }
+    // for (const originalProperty in panelProperties) {
+    //     if (properties.hasOwnProperty(originalProperty)) {
+    //         const displayName = panelProperties[originalProperty];
+    //         infoPanelContent += `<li><strong>${displayName}:</strong> ${properties[originalProperty]}</li>`;
+    //     }
+    // }
+    infoPanelContent += '</ul>';
+    // Set the info panel content
+    document.getElementById('feature-properties').innerHTML = infoPanelContent;
+});
+
+map.on('mousemove', 'housing', (e) => {
+    // Generate the content for the popup
+    const properties = e.features[0].properties;
+    let popupContent = '<h3>Basic Information</h3><ul>';
+    
+    for (const originalProperty in propertiesToDisplay) {
+        if (properties.hasOwnProperty(originalProperty)) {
+            const displayName = propertiesToDisplay[originalProperty];
+            popupContent += `<li>${displayName}: ${properties[originalProperty]}</li>`;
+        }
+    }
+    popupContent += '</ul>';
+
+
+    // Instead of adding the popup to the map here, just set its content and location
+    popup.setLngLat(e.lngLat)
+        .setHTML(popupContent);
+
+    
+});
+
+
+map.on('mouseenter', 'housing', () => {
+    map.getCanvas().style.cursor = 'pointer';
+    popup.addTo(map); // Add the popup to the map when the cursor enters a feature
+});
+
+map.on('mouseleave', 'housing', () => {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+});
