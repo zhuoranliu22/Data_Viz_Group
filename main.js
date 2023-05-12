bd3363c2cb95e8505e1bcf97cb34bf1da4878877
zhuoranliu22 <zhuoran.liu.22@ucl.ac.uk>
2023-05-12 23:14:00 +0100

    lzr

--- main.js
+++ main.js
@@ -11,15 +11,6 @@ const map = new mapboxgl.Map({
 
 
 map.on('load', () => {
-    // map.addLayer({
-    //     id: 'background',
-    //     type: 'background',
-    //     paint: {
-    //         'background-color': 'rgba(128,128,128,0.7)' // Grey color
-    //         // 'background-opacity': 0.5
-    //     }
-        
-    // });
 
     map.addSource('data_map', {
         type: 'geojson',
@@ -42,9 +33,7 @@ map.on('load', () => {
                 'Highest', '#011627',
                 '#000000' // fallback color if none of the categories match
             ],
-            'fill-opacity': 0.8
-            
-            
+            'fill-opacity': 1                   
         }
     });
 
@@ -64,25 +53,131 @@ map.on('load', () => {
                 'Highest', '#626c41',
                 '#000000' // fallback color if none of the categories match
             ],
-            'fill-opacity': 0
-            
-            
+            'fill-opacity': 0           
         }
     });
+    
+    map.addLayer({
+        id: 'market',
+        type: 'fill',
+        source: 'data_map',
+        layout: {},
+        paint: {
+            'fill-color': [
+                'match',
+                ['get', 'MrktPrs'],
+                'Lowest', '#F9FBE7',
+                'Lower', '#E7CBCB',
+                'Intermediate', '#C88EA7',
+                'Higher', '#99627A',
+                'Highest', '#643843',
+                '#000000' // fallback color if none of the categories match
+            ],
+            'fill-opacity': 0           
+        }
+    });
+
+    map.addLayer({
+        id: 'popu',
+        type: 'fill',
+        source: 'data_map',
+        layout: {},
+        paint: {
+            'fill-color': [
+                'match',
+                ['get', 'PpltnVl'],
+                'Lowest', '#EEEEEE',
+                'Lower', '#FFEAD2',
+                'Intermediate', '#DBDFEA',
+                'Higher', '#ACB1D6',
+                'Highest', '#8294C4',
+                '#000000' // fallback color if none of the categories match
+            ],
+            'fill-opacity': 0           
+        }
+    });
+    var legend = document.getElementById('legend');
+    legend.innerHTML = '';
+    // legend.innerHTML = '<h3>Displacement Risk Index</h3>';
+    var disgrades = ['Lowest', 'Lower', 'Intermediate', 'Higher', 'Highest'];
+    var discolors = ['#B3CDE0', '#6897BB', '#1F78B4', '#053061', '#011627'];   
+    // loop through our intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < disgrades.length; i++) {
+        legend.innerHTML +=
+            '<i style="background:' + discolors[i] + '; width: 14px; height: 14px; display: inline-block; margin-right: 2px;"></i> ' +
+            disgrades[i] + '<br>';
+    }
 });
 
 
 document.getElementById("displacement").addEventListener("click", function(){
     map.setPaintProperty('displacement','fill-opacity',1);
     map.setPaintProperty('housing','fill-opacity',0);
+    map.setPaintProperty('market','fill-opacity',0);
+    map.setPaintProperty('popu','fill-opacity',0);
+    var legend = document.getElementById('legend');
+    legend.innerHTML = '';
+    // legend.innerHTML = '<h3>Displacement Risk Index</h3>';
+    var disgrades = ['Lowest', 'Lower', 'Intermediate', 'Higher', 'Highest'];
+    var discolors = ['#B3CDE0', '#6897BB', '#1F78B4', '#053061', '#011627'];   
+    // loop through our intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < disgrades.length; i++) {
+        legend.innerHTML +=
+            '<i style="background:' + discolors[i] + '; width: 14px; height: 14px; display: inline-block; margin-right: 2px;"></i> ' +
+            disgrades[i] + '<br>';
+    }
 });
-    
+
 document.getElementById("housing").addEventListener("click", function(){
     map.setPaintProperty('displacement','fill-opacity',0);
     map.setPaintProperty('housing','fill-opacity',1);
+    map.setPaintProperty('market','fill-opacity',0);
+    map.setPaintProperty('popu','fill-opacity',0);
+    var legend = document.getElementById('legend');
+    legend.innerHTML = '';
+    var hou_grades = ['Lowest', 'Lower', 'Intermediate', 'Higher', 'Highest'];
+    var hou_colors = ['#4d5d53', '#729d80', '#738276', '#9eb08d', '#626c41'];   
+    // loop through our intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < hou_grades.length; i++) {
+        legend.innerHTML +=
+            '<i style="background:' + hou_colors[i] + '; width: 14px; height: 14px; display: inline-block; margin-right: 2px;"></i> ' +
+            hou_grades[i] + '<br>';
+    }
 });
 
+document.getElementById('market').addEventListener("click", function(){
+    map.setPaintProperty('displacement','fill-opacity',0);
+    map.setPaintProperty('housing','fill-opacity',0);
+    map.setPaintProperty('market','fill-opacity',1);
+    map.setPaintProperty('popu','fill-opacity',0);
+    var legend = document.getElementById('legend');
+    legend.innerHTML = '';
+    var mar_grades = ['Lowest', 'Lower', 'Intermediate', 'Higher', 'Highest'];
+    var mar_colors = ['#F9FBE7', '#E7CBCB', '#C88EA7', '#99627A', '#643843'];   
+    // loop through our intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < mar_grades.length; i++) {
+        legend.innerHTML +=
+            '<i style="background:' + mar_colors[i] + '; width: 14px; height: 14px; display: inline-block; margin-right: 2px;"></i> ' +
+            mar_grades[i] + '<br>';
+    }
+});
 
+document.getElementById("popu").addEventListener("click", function(){
+    map.setPaintProperty('displacement','fill-opacity',0);
+    map.setPaintProperty('housing','fill-opacity',0);
+    map.setPaintProperty('market','fill-opacity',0);
+    map.setPaintProperty('popu','fill-opacity',1);
+    var legend = document.getElementById('legend');
+    legend.innerHTML = '';
+    var pop_grades = ['Lowest', 'Lower', 'Intermediate', 'Higher', 'Highest'];
+    var pop_colors = ['#EEEEEE', '#FFEAD2', '#DBDFEA', '#ACB1D6', '#8294C4'];   
+    // loop through our intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < pop_grades.length; i++) {
+        legend.innerHTML +=
+            '<i style="background:' + pop_colors[i] + '; width: 14px; height: 14px; display: inline-block; margin-right: 2px;"></i> ' +
+            pop_grades[i] + '<br>';
+    }
+});
 
 const popup = new mapboxgl.Popup({
     closeButton: false,
@@ -90,9 +185,11 @@ const popup = new mapboxgl.Popup({
 });
 
 const propertiesToDisplay = {
-    'NTAName': 'Name',
+    
     'DsplcRI': 'Displacement Risk',
-    'HsngCnd': 'Housing Conditions'
+    'HsngCnd': 'Housing Conditions',
+    'MrktPrs': 'Market Pressure',
+    'PpltnVl': 'Population Vulnerability'
     
 };
 
@@ -103,106 +200,165 @@ const panelProperties = {
     // Add more properties as needed
 };
 
-map.on('click', 'displacement', (e) => {
+//map.on('click', 'displacement', (e) => {
     // Get the properties of the clicked feature
-    const properties = e.features[0].properties;
+    //const properties = e.features[0].properties;
     // Generate the content for the info panel
-    let infoPanelContent = '<h3>Feature Information</h3><ul>';
-    for (const property in properties) {
-        infoPanelContent += `<li>${property}: ${properties[property]}</li>`;
-    }
+    //let infoPanelContent = '<h3>Feature Information</h3><ul>';
+    //for (const property in properties) {
+     //   infoPanelContent += `<li>${property}: ${properties[property]}</li>`;
+    //}
     // for (const originalProperty in panelProperties) {
     //     if (properties.hasOwnProperty(originalProperty)) {
     //         const displayName = panelProperties[originalProperty];
     //         infoPanelContent += `<li><strong>${displayName}:</strong> ${properties[originalProperty]}</li>`;
     //     }
     // }
-    infoPanelContent += '</ul>';
-    // Set the info panel content
-    document.getElementById('feature-properties').innerHTML = infoPanelContent;
+//     infoPanelContent += `<p style="font-size:18px;">The selected area is <strong>${properties.NTAName}</strong>.</p>`;
+//     infoPanelContent += `<p style="font-size:16px;">The Displacement Risk Index is <strong>${properties.DsplcRI}</strong>.</p>`;
+//     document.getElementById('feature-properties').innerHTML = infoPanelContent;
+// });
+document.getElementById('back-button').addEventListener('click', function() {
+    // Hide the back button
+    this.style.display = 'none';
+    // Clear the info panel
+    document.getElementById('info-panel').innerHTML = '';
+    // Show the welcome page
+    document.getElementById('feature-properties').style.display = 'block';
 });
 
-map.on('mousemove', 'displacement', (e) => {
-    // Generate the content for the popup
-    const properties = e.features[0].properties;
-    let popupContent = '<h3>Basic Information</h3><ul>';
+map.on('click', function(e) {
+    var properties = map.queryRenderedFeatures(e.point)[0].properties;
+    var infoPanelContent = '';
+
+    // Always show the same data
+    infoPanelContent = '<h2>' + properties.NTAName + '</h2>';
+    infoPanelContent += '<p>The displacement risk index is ' + properties.DsplcRI + '</p>';
+    infoPanelContent += '<p>The displacement risk index is ' + properties.PpltnVl + '</p>';
+    // ... add more data as needed
+
+    document.getElementById('info-panel').innerHTML = infoPanelContent;
+    document.getElementById('back-button').style.display = 'block';
+});
+
+
+
+
+// map.on('mousemove', 'displacement', (e) => {
+//     // Generate the content for the popup
+//     const properties = e.features[0].properties;
+//     let popupContent = '<h3>Basic Information</h3><ul>';
     
-    for (const originalProperty in propertiesToDisplay) {
-        if (properties.hasOwnProperty(originalProperty)) {
-            const displayName = propertiesToDisplay[originalProperty];
-            popupContent += `<li>${displayName}: ${properties[originalProperty]}</li>`;
+//     for (const originalProperty in propertiesToDisplay) {
+//         if (properties.hasOwnProperty(originalProperty)) {
+//             const displayName = propertiesToDisplay[originalProperty];
+//             popupContent += `<li>${displayName}: ${properties[originalProperty]}</li>`;
+//         }
+//     }
+//     popupContent += '</ul>';
+
+
+//     // Instead of adding the popup to the map here, just set its content and location
+//     popup.setLngLat(e.lngLat)
+//         .setHTML(popupContent);
+
+    
+// });
+
+
+// map.on('mouseenter', 'displacement', () => {
+//     map.getCanvas().style.cursor = 'pointer';
+//     popup.addTo(map); // Add the popup to the map when the cursor enters a feature
+// });
+
+// map.on('mouseleave', 'displacement', () => {
+//     map.getCanvas().style.cursor = '';
+//     popup.remove();
+// });
+
+
+
+// map.on('click', 'housing', (e) => {
+//     // Get the properties of the clicked feature
+//     const properties = e.features[0].properties;
+//     // Generate the content for the info panel
+//     let infoPanelContent = '<h3>Feature Information</h3><ul>';
+//     for (const property in properties) {
+//         infoPanelContent += `<li>${property}: ${properties[property]}</li>`;
+//     }
+//     // for (const originalProperty in panelProperties) {
+//     //     if (properties.hasOwnProperty(originalProperty)) {
+//     //         const displayName = panelProperties[originalProperty];
+//     //         infoPanelContent += `<li><strong>${displayName}:</strong> ${properties[originalProperty]}</li>`;
+//     //     }
+//     // }
+//     infoPanelContent += '</ul>';
+//     // Set the info panel content
+//     document.getElementById('feature-properties').innerHTML = infoPanelContent;
+// });
+
+// map.on('mousemove', 'housing', (e) => {
+//     // Generate the content for the popup
+//     const properties = e.features[0].properties;
+//     let popupContent = '<h3>Basic Information</h3><ul>';
+    
+//     for (const originalProperty in propertiesToDisplay) {
+//         if (properties.hasOwnProperty(originalProperty)) {
+//             const displayName = propertiesToDisplay[originalProperty];
+//             popupContent += `<li>${displayName}: ${properties[originalProperty]}</li>`;
+//         }
+//     }
+//     popupContent += '</ul>';
+
+
+//     // Instead of adding the popup to the map here, just set its content and location
+//     popup.setLngLat(e.lngLat)
+//         .setHTML(popupContent);
+
+    
+// });
+
+
+// map.on('mouseenter', 'housing', () => {
+//     map.getCanvas().style.cursor = 'pointer';
+//     popup.addTo(map); // Add the popup to the map when the cursor enters a feature
+// });
+
+// map.on('mouseleave', 'housing', () => {
+//     map.getCanvas().style.cursor = '';
+//     popup.remove();
+// });
+
+function setupLayerMouseEffects(layerName) {
+    map.on('mousemove', layerName, (e) => {
+        const properties = e.features[0].properties;
+        let popupContent = '<h3>' + properties.NTAName + '</h3><ul>';
+        
+        for (const originalProperty in propertiesToDisplay) {
+            if (properties.hasOwnProperty(originalProperty)) {
+                const displayName = propertiesToDisplay[originalProperty];
+                popupContent += `<li>${displayName}: ${properties[originalProperty]}</li>`;
+            }
         }
-    }
-    popupContent += '</ul>';
+        popupContent += '</ul>';
 
+        popup.setLngLat(e.lngLat).setHTML(popupContent);
+    });
 
-    // Instead of adding the popup to the map here, just set its content and location
-    popup.setLngLat(e.lngLat)
-        .setHTML(popupContent);
+    map.on('mouseenter', layerName, () => {
+        map.getCanvas().style.cursor = 'pointer';
+        popup.addTo(map);
+    });
 
-    
-});
+    map.on('mouseleave', layerName, () => {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+}
 
+// Now you can call setupLayerMouseEffects for each of your layers
+setupLayerMouseEffects('displacement');
+setupLayerMouseEffects('housing');
+setupLayerMouseEffects('market');
+setupLayerMouseEffects('popu');
 
-map.on('mouseenter', 'displacement', () => {
-    map.getCanvas().style.cursor = 'pointer';
-    popup.addTo(map); // Add the popup to the map when the cursor enters a feature
-});
-
-map.on('mouseleave', 'displacement', () => {
-    map.getCanvas().style.cursor = '';
-    popup.remove();
-});
-
-
-
-map.on('click', 'housing', (e) => {
-    // Get the properties of the clicked feature
-    const properties = e.features[0].properties;
-    // Generate the content for the info panel
-    let infoPanelContent = '<h3>Feature Information</h3><ul>';
-    for (const property in properties) {
-        infoPanelContent += `<li>${property}: ${properties[property]}</li>`;
-    }
-    // for (const originalProperty in panelProperties) {
-    //     if (properties.hasOwnProperty(originalProperty)) {
-    //         const displayName = panelProperties[originalProperty];
-    //         infoPanelContent += `<li><strong>${displayName}:</strong> ${properties[originalProperty]}</li>`;
-    //     }
-    // }
-    infoPanelContent += '</ul>';
-    // Set the info panel content
-    document.getElementById('feature-properties').innerHTML = infoPanelContent;
-});
-
-map.on('mousemove', 'housing', (e) => {
-    // Generate the content for the popup
-    const properties = e.features[0].properties;
-    let popupContent = '<h3>Basic Information</h3><ul>';
-    
-    for (const originalProperty in propertiesToDisplay) {
-        if (properties.hasOwnProperty(originalProperty)) {
-            const displayName = propertiesToDisplay[originalProperty];
-            popupContent += `<li>${displayName}: ${properties[originalProperty]}</li>`;
-        }
-    }
-    popupContent += '</ul>';
-
-
-    // Instead of adding the popup to the map here, just set its content and location
-    popup.setLngLat(e.lngLat)
-        .setHTML(popupContent);
-
-    
-});
-
-
-map.on('mouseenter', 'housing', () => {
-    map.getCanvas().style.cursor = 'pointer';
-    popup.addTo(map); // Add the popup to the map when the cursor enters a feature
-});
-
-map.on('mouseleave', 'housing', () => {
-    map.getCanvas().style.cursor = '';
-    popup.remove();
-});
